28a656766144c52edf2cf30a4986b1e4275df853
UJJWAL <ujjwal8851114020@gmail.com>
2024-08-24 11:37:42 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import firebase from 'firebase/app'
@@ -56,13 +56,26 @@ function ChatRoom(){
   const messageRef=firestore.collection('messages');
   const query=messageRef.orderBy('createdAt').limit(25);
   const [messages]=useCollectionData(query,{idField:'id'});
+  const [formValue,setFormValue]=useState('');
+  const sendMessage=async()=>{
+    e.preventDefault();
+    const {uid,photoURL}=auth.currentUser;
+    await messageRef.add({
+      text:formValue,
+      createdAt:firebase.firestore.FieldValue.serverTimeStamp(),
+      uid,
+      photoURL
+    });
+    setFormValue('');
+  }
   return(
     <>
     <div>
       {messages && messages.map(msg=><ChatMessage key={msg.id} message={msg} />)}
     </div>
-    <form>
-      
+    <form onSubmit={sendMessage}>
+      <input value={formValue} onChange={(e)=>setFormValue(e.target.value)} />
+      <button type='submit'>Submit</button>
     </form>
     </>
   )
